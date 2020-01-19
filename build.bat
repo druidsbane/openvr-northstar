7bd10dfc9870001d19551ab7b1afc540e951d33d
Hesham Wahba <hesham.wahba@gmail.com>
2020-01-19 01:37:17 -0500

    Make build work with submodule and SteamVR tag v1.9.16

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-git clone --depth 1 https://github.com/ValveSoftware/openvr.git ../openvr
+REM git clone --depth 1 https://github.com/ValveSoftware/openvr.git ../openvr
+git checkout --recurse-submodules
+git submodule update --init --recursive
 cmake -B build .
 cmake --build ./build -- /t:restore,Build
